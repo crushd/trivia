2272a887d0eb5a929a88dc739d4d840e23e14268
Cody Rush <codyrush@gmail.com>
2018-12-17 22:43:02 -0800

    added correct answer in random location with other possible answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,12 @@
+// get a random number between a minimum and maximum value
+function getRandomNumber(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 function getQuestion() {
 
     // Get a random question
-    var queryURL = "https://opentdb.com/api.php?amount=1";
+    var queryURL = "https://opentdb.com/api.php?amount=1&type=multiple";
 
     $.ajax({
         url: queryURL,
@@ -9,13 +14,26 @@ function getQuestion() {
     }).then(function(response) {
         //var questionArray = response;
 
-        console.log(response.results[0].question);
+        //console.log(response.results[0].question);
         $("#triviaQuestion").html(response.results[0].question);
 
         var answerList = [];
-        answerList.push(response.results[0].correct_answer);
-        answerList.push(response.results[0].incorrect_answers);
-        console.log("Answers: " + answerList);
+
+        var incorrectAnswers = response.results[0].incorrect_answers;
+
+        for (i=0;i<incorrectAnswers.length;i++) {
+            answerList.push(incorrectAnswers[i]);
+        }
+
+        var correctAnswer = response.results[0].correct_answer;
+        answerList.splice(getRandomNumber(0,answerList.length),0,correctAnswer);
+
+        $("#answer1").html(answerList[0]);
+        $("#answer2").html(answerList[1]);
+        $("#answer3").html(answerList[2]);
+        $("#answer4").html(answerList[3]);
+
+        console.log("Answers: " + answerList + ". Correct: " + correctAnswer);
     });
 
 }
